79f9cbe8ffb3076adf69ee076408f6c5036d7b7d
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-05-03 09:38:43 -0400

    Initial Commit

---
+++ Voxy/VoxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoxyApp.swift
+//  Voxy
+//
+//  Created by Andy Stewart on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct VoxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
